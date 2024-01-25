9415a71755091611fdd1d3faf62e5fe0cea077b9
Fardin Mustaque <fardindeveloper1@gmail.com>
2024-01-25 20:27:10 +0530

    css up

--- frontend/src/Failure.jsx
+++ frontend/src/Failure.jsx
@@ -5,14 +5,14 @@ import { useLocation } from 'react-router-dom';
 
 const Failure = () => {
   const { state } = useLocation();
-  const errorMessage = state?.message || "An error occurred.";
+  const message = state?.message || "An error occurred.";
+  const data = state?.data || "An error occurred.";
   return (
     <div className="failure-container">
       <div className="failure-box">
         <div className="cross-mark">&#10008;</div>
-        <h2>Operation Failed</h2>
-        <p>Sorry, something went wrong. Please try again later.</p>
-        <p>{errorMessage}</p>
+        <h2>{message}</h2>
+        <p>{{data}}.</p>
 
         {/* You can add more information or buttons if needed */}
       </div>
